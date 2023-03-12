11570a79291e58b2b0597facc0f981c97c646460
lucaa21 <ringuet.luca@gmail.com>
2023-03-12 18:20:04 +0100

    maj

---
+++ server/eshops/circlesportswear.js
@@ -0,0 +1,57 @@
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+
+/**
+ * Parse webpage e-shop
+ * @param  {String} data - html response
+ * @return {Array} products
+ */
+const parse = data => {
+  const $ = cheerio.load(data);
+
+  return $('.product-grid-container .grid__item')
+    .map((i, element) => {
+      var name = $(element)
+        .find('.full-unstyled-link')
+        .text()
+        .trim()
+        .split('\n');
+      name = name[0];
+      var caracteristique = $(element)
+      .find('.card__characteristic')
+      .text()
+      .trim()
+      .replace(/\s/g, ' ');
+      caracteristique = caracteristique.slice(0,caracteristique.length /2)
+      var price = $(element)
+          .find('.money')
+          .text();
+      price = price.slice(0,price.length /2);
+      return {name, price, caracteristique};
+    })
+    .get();
+};
+
+/**
+ * Scrape all the products for a given url page
+ * @param  {[type]}  url
+ * @return {Array|null}
+ */
+module.exports.scrape = async url => {
+  try {
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const body = await response.text();
+
+      return parse(body);
+    }
+
+    console.error(response);
+
+    return null;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
