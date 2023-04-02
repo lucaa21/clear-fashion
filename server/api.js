8568eff6b90121715df2a240fcb629cb3780f3e5
lucaa21 <ringuet.luca@gmail.com>
2023-04-02 13:35:55 +0200

    maj

--- server/api.js
+++ server/api.js
@@ -14,55 +14,28 @@ app.use(helmet());
 
 app.options('*', cors());
 
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const fs = require('fs');
+const { Console } = require('console');
+
+function getClient() {
+  const uri = "mongodb+srv://ringuetluca:mdpmdpmdp@cluster0.nxupqlp.mongodb.net/test?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+  return client;
+}
+
 app.get('/', (request, response) => {
   response.send({'ack': true});
 });
 
-//
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const fs = require('fs');
-
-function getClient() {
-    const uri = "mongodb+srv://ringuetluca:mdpmdpmdp@Cluster0.nxupqlp.mongodb.net/test";
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-    return client;
-}
-
-app.get('/products/search', async (request, response) => {
-  try{
-    const client = getClient();
-    const collection = client.db("Cluster0").collection("general");
-    const brandName = request.query.brand;
-	  const priceRoof = request.query.price;
-	  var limPage = request.query.limit;
-
-    var script = {};
-
-    if (limPage == undefined) {
-      limPage = 12;
-    } else {
-      limPage = parseInt(limPage);
-    }
-    if (brandName !== undefined) {
-      script.brand = brandName;
-    }
-    if (priceRoof !== undefined) {
-      script.price = {$lte: parseInt(priceRoof)};
-    }
-    const found = await collection.find(script).limit(limPage).toArray();
-    response.send({result: found});
-
-  } catch(err) {
-    response.send({error : "Unreachable information"});  
-  }
-});
 
 app.get('/brands', async (request, response) => {
   try{
     const client = getClient();
     const collection = client.db("Cluster0").collection("general");
     const found = await collection.distinct('brand');
-    response.send({result: found});
+    response.send({brands: found});
   }
   catch{
     response.send({error : "Couldn't fetch brands"}); 
@@ -76,16 +49,13 @@ app.get('/products/:id', async (request, response) => {
     const client = getClient();
     const collection = client.db("Cluster0").collection("general");
     const found = await collection.find(script).toArray();
-    
-    response.send({result: found});
+    const metadata = {pageSize: found.length};
+    response.send({result: found, meta: metadata});
 
   } catch(err) {
 	  response.send({error : "ID not found"});  
   }
 });
-
-
-//
 
 app.listen(PORT);
 
