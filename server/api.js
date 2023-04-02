d4e1ab922bb488ba8431cd791458b0b1d94ce12d
lucaa21 <ringuet.luca@gmail.com>
2023-04-02 22:17:38 +0200

    test

--- server/api.js
+++ server/api.js
@@ -4,6 +4,16 @@ const helmet = require('helmet');
 
 const PORT = 8092;
 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const fs = require('fs');
+const { Console } = require('console');
+
+function getClient() {
+  const uri = "mongodb+srv://tatouti:MongoDB6@clusterclearfashion.iyacjoa.mongodb.net/test?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+  return client;
+}
+
 const app = express();
 
 module.exports = app;
@@ -14,75 +24,113 @@ app.use(helmet());
 
 app.options('*', cors());
 
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const fs = require('fs');
-const { Console } = require('console');
-
-function getClient() {
-  const uri = "mongodb+srv://ringuetluca:mdpmdpmdp@cluster0.nxupqlp.mongodb.net/test?retryWrites=true&w=majority";
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-  return client;
-}
-
 app.get('/', (request, response) => {
-  response.send({'ack': true});
+  response.send({'ack': false});
 });
-
-app.get('/products/search', async (request, response) => {
-  try{
-    const client = getClient();
-    const collection = client.db("Cluster0").collection("general");
-    const brand = request.query.brand;
-	  const price = request.query.price;
-	  var limPage = request.query.limit;
-
-    var script = {};
-
-    if (limPage == undefined) {
-      limPage = 12;
-    } else {
-      limPage = parseInt(limPage);
-    }
-    if (brand !== undefined) {
-      script.brand = brand;
-    }
-    if (price !== undefined) {
-      script.price = {$lte: parseInt(price)};
-    }
-    const found = await collection.find(script).toArray();
-    const metadata = {currentPage: 1,
-                      pageSize: limPage,
-                      pageCount: parseInt(found.length/limPage)}
-    response.send({result: found, meta: metadata});
-
-  } catch(err) {
-    response.send({error : "Unreachable information"});  
-  }
-});
-
 
 app.get('/brands', async (request, response) => {
   try{
     const client = getClient();
-    const collection = client.db("Cluster0").collection("general");
+    const collection = client.db("ClusterClearFashion").collection("GENERAL");
     const found = await collection.distinct('brand');
-    response.send({brands: found});
+    //response.send({brands: found});
+    response.json(found);
   }
   catch{
     response.send({error : "Couldn't fetch brands"}); 
   }
 });
 
-app.get('/products/:id', async (request, response) => {
+app.get('/products', async (request, response) => {
+  const client = getClient();
+  const collection = client.db("ClusterClearFashion").collection("GENERAL");
+
+  const result = await collection.find({}).toArray();
+
+  response.json(result);
+});
+
+app.get('/sort', async (request, response) => {
+  const client = getClient();
+  const collection = client.db("ClusterClearFashion").collection("GENERAL");
+  var sortVal = request.query.sort;
+
+  const sortType ={};
+  if(sortVal==1){
+    sortType.price = 1;
+  }
+  else if(sortVal==-1){
+    sortType.price = -1;
+  }
+  else{
+    sortType.price = 0;
+  }
+
+  const result = await collection.find({}).sort(sortType).toArray();
+
+  response.json(result);
+});
+
+app.get('/products/search', async (request, response) => {
+  try{
+    const client = getClient();
+    const collection = client.db("ClusterClearFashion").collection("GENERAL");
+
+    var script ={};
+    var page = request.query.page;
+    var limit = request.query.limit;
+    var price = request.query.price;
+    var brand = request.query.brand;
+
+    if(page == undefined){
+      page = 1;
+    }
+    else{
+      page = parseInt(page);
+    }
+
+    if(limit == undefined){
+      limit = 12;
+    }
+    else{
+      limit = parseInt(limit);
+    }
+    const skip = (page - 1) * limit;
+
+    if((brand!="")){
+      script.brand = brand;
+    }
+
+    if(price!=""){
+      script.price = {$lte: parseFloat(price)};
+    }
+
+    const count = await collection.countDocuments(script);
+    const totalPages = Math.ceil(count / limit);
+
+    const result = await collection.find(script).skip(skip).limit(limit).toArray();
+
+    response.json({
+      currentPage: page,
+      totalPages: totalPages,
+      totalCount: count,
+      data: result
+    });
+  }
+  catch{
+    response.send({error : "Couldn't fetch searchs"}); 
+  }
+});
+
+app.get('/products/id', async (request, response) => {
   try{
     const productId = request.params.id;
     const script = {_id: ObjectId(productId)};
     const client = getClient();
-    const collection = client.db("Cluster0").collection("general");
+    const collection = client.db("ClusterClearFashion").collection("GENERAL");
     const found = await collection.find(script).toArray();
-    const metadata = {pageSize: found.length};
-    response.send({result: found, meta: metadata});
+    
+    response.send({ids: found});
 
   } catch(err) {
 	  response.send({error : "ID not found"});  
